5a5d05a6d0df0e9809461f6b618f31a96c1b17f4
김민지 <39117025+mjkim103301@users.noreply.github.com>
2019-02-01 14:30:19 +0900

    02/01

--- CarDrive_1/Car.cs
+++ CarDrive_1/Car.cs
@@ -2,12 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+using System.Threading;
+
 
 namespace CarDrive_1
 {
-    class Car
+    class TimerExample
     {
-        //car.cs 파일 수정수정수정
+        public void Timer()
+        {
+            var autoEvent = new AutoResetEvent(false);
+            var statusChecker = new StatusChecker(10);
+
+            var stateTimer = new Timer(statusChecker.CheckStatus, autoEvent, 1000, 250);
+
+        }
+    }
+    class StatusChecker
+    {
+        private int invokeCount;
+        private int maxCount;
+        public StatusChecker(int count)
+        {
+            invokeCount = 0;
+            maxCount = count;
+        }
+        public void CheckStatus(object stateInfo)
+        {
+            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+            
+        }
+    }
+    class Car
+    {  
+        
+       
+        /// <summary>
+        /// 속도, 가속도 설정
+        /// </summary>
+        /// <param name="">
+        /// 
+        /// </param>
+        public void input()
+        {
+          
+            double accel = 9.8;         
+            double velocity= 0.0;
+            velocity= velocity+accel;
+
+            //velocity = velocity_0 + accel * duration;
+        }
+         
+        
+
+        
+        //방향키 입력으로 위치이동
+        public void Key(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            int x = 10, y = 10;
+            while(true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+               
+                cki = Console.ReadKey(true);
+                switch(cki.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        x++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        y++;
+                        break;
+                    case ConsoleKey.Q:
+                        return;
+                   
+                       
+                }
+            }
+        }
+
     }
 }
